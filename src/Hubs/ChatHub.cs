4e22914193566b90c852a6bd6c70e742d354748d
payneqin <qinyuanpei@163.com>
2020-01-21 18:13:01 +0800

    使用Redis

--- src/Hubs/ChatHub.cs
+++ src/Hubs/ChatHub.cs
@@ -1,69 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.SignalR;
-using Microsoft.AspNetCore.Http;
-using CSRedis;
+using System.Collections;
 using System.Security.Claims;
+using System.Threading.Tasks;
+using CSRedis;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.SignalR;
 
-namespace SignalR.Distributed.Hubs
-{
-    public class ChatHub : Hub
-    {
+namespace SignalR.Distributed.Hubs {
+    public class ChatHub : Hub {
         /// <summary>
         /// HttpContext
         /// </summary>
-        private HttpContext _httpContext => this.Context.GetHttpContext();
+        private HttpContext _httpContext => this.Context.GetHttpContext ();
 
         /// <summary>
-        /// 用户列表
+        /// CSRedisClient
         /// </summary>
-        private Dictionary<string, string> _userList = new Dictionary<string, string>();
-
-        /// <summary>
-        /// IRedisClient
-        /// </summary>
-        private readonly IRedisClient _redisClient;
-
-        public ChatHub(IRedisClient redisClient)
-        {
-            _redisClient = redisClient;
-        }
+        private readonly CSRedisClient _redisClient = new CSRedis.CSRedisClient ("127.0.0.1:6379");
 
         /// <summary>
         /// 用户连接
         /// </summary>
         /// <returns></returns>
-        public override Task OnConnectedAsync()
-        {
+        public override Task OnConnectedAsync () {
             var userId = _httpContext.Request.Query["UserId"][0];
-            if (string.IsNullOrEmpty(userId))
+            if (string.IsNullOrEmpty (userId))
                 userId = Context.ConnectionId;
             var roomId = _httpContext.Request.Query["RoomId"][0];
-            _userList.Add(userId, Context.ConnectionId);
-            Groups.AddToGroupAsync(Context.ConnectionId, roomId);
-            if (!string.IsNullOrEmpty(roomId))
-            {
-                var message = new
-                {
+            BindUserId(userId);
+            Groups.AddToGroupAsync (Context.ConnectionId, roomId);
+            if (!string.IsNullOrEmpty (roomId)) {
+                var message = new {
                     EventTime = DateTime.Now,
                     EventType = "系统消息",
                     UserID = userId,
                     Content = $"{DateTime.Now},用户{userId}加入聊天室{roomId}"
                 };
-                return Clients.Group(roomId).SendAsync("UserJoined", message);
-            }
-            else
-            {
-                var message = new
-                {
+                return Clients.Group (roomId).SendAsync ("UserJoined", message);
+            } else {
+                var message = new {
                     EventTime = DateTime.Now,
                     EventType = "系统消息",
                     UserID = userId,
                     Content = $"{DateTime.Now},用户{userId}加入聊天"
                 };
-                return Clients.All.SendAsync("UserJoined", message);
+                return Clients.All.SendAsync ("UserJoined", message);
             }
         }
 
@@ -72,35 +55,29 @@ namespace SignalR.Distributed.Hubs
         /// </summary>
         /// <param name="exception"></param>
         /// <returns></returns>
-        public override Task OnDisconnectedAsync(Exception exception)
-        {
+        public override Task OnDisconnectedAsync (Exception exception) {
             var userId = _httpContext.Request.Query["UserId"][0];
-            if (string.IsNullOrEmpty(userId))
+            if (string.IsNullOrEmpty (userId))
                 userId = Context.ConnectionId;
             var roomId = _httpContext.Request.Query["RoomId"][0];
-            _userList.Remove(userId);
-            Groups.RemoveFromGroupAsync(Context.ConnectionId, roomId);
-            if (!string.IsNullOrEmpty(roomId))
-            {
-                var message = new
-                {
+            UnbindUserId(userId);
+            Groups.RemoveFromGroupAsync (Context.ConnectionId, roomId);
+            if (!string.IsNullOrEmpty (roomId)) {
+                var message = new {
                     EventTime = DateTime.Now,
                     EventType = "系统消息",
                     UserID = userId,
                     Content = $"{DateTime.Now},用户{userId}退出聊天室{roomId}"
                 };
-                return Clients.Group(roomId).SendAsync("UserLeaved", message);
-            }
-            else
-            {
-                var message = new
-                {
+                return Clients.Group (roomId).SendAsync ("UserLeaved", message);
+            } else {
+                var message = new {
                     EventTime = DateTime.Now,
                     EventType = "系统消息",
                     UserID = userId,
                     Content = $"{DateTime.Now},用户{userId}退出聊天"
                 };
-                return Clients.All.SendAsync("UserLeaved", message);
+                return Clients.All.SendAsync ("UserLeaved", message);
             }
         }
 
@@ -110,13 +87,12 @@ namespace SignalR.Distributed.Hubs
         /// <param name="userId">用户Id</param>
         /// <param name="message">消息</param>
         /// <returns></returns>
-        public Task SendTo(string userId, string message, string sendBy)
-        {
-            return Clients.User(userId).SendAsync("ReceiveMessage", new
-            {
+        public Task SendTo (string userId, string message, string sendBy) {
+            var connIds = GetBindedIds(userId);
+            return Clients.Clients(connIds.ToList()).SendAsync ("ReceiveMessage", new {
                 EventTime = DateTime.Now,
-                EventSender = sendBy,
-                Content = message
+                    EventSender = sendBy,
+                    Content = message
             });
         }
 
@@ -125,14 +101,31 @@ namespace SignalR.Distributed.Hubs
         /// </summary>
         /// <param name="message">消息</param>
         /// <returns></returns>
-        public Task SendAll(string message, string sendBy)
-        {
-            return Clients.All.SendAsync("ReceiveMessage", new
-            {
+        public Task SendAll (string message, string sendBy) {
+            return Clients.Others.SendAsync ("ReceiveMessage", new {
                 EventTime = DateTime.Now,
-                EventSender = sendBy,
-                Content = message
+                    EventSender = sendBy,
+                    Content = message
             });
+        }
+
+        private void BindUserId (string userId) {
+            var cacheKey = $"SignalR_User_{userId}";
+            _redisClient.RPush (cacheKey, Context.ConnectionId);
+        }
+
+        private void UnbindUserId (string userId) 
+        {
+            var cacheKey = $"SignalR_User_{userId}";
+            var connIds = _redisClient.LRange (userId, 0, -1);
+            _redisClient.RPop(userId);
+            connIds.ToList().Remove(Context.ConnectionId);
+            _redisClient.LPush<string>(cacheKey,connIds);
+        }
+
+        private string[] GetBindedIds (string userId) {
+            var cacheKey = $"SignalR_User_{userId}";
+            return _redisClient.LRange (cacheKey, 0, -1);
         }
     }
 }
